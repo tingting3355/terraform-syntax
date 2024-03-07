a6fcd99f5f094e632c214e694ff19c46ddc4d08c
tingting3355 <skysorahwi@gamil.com>
2024-03-07 14:20:11 +0900

    작업완료

--- main.tf
+++ main.tf
@@ -12,31 +12,12 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "main"
-  }
+# 개발환경
+module "default_custome_vpc" {
+  source = "./custom_vpc"
 }
 
-# Create a Public Subnet
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = aws_vpc.default.id
-  cidr_block        = "10.0.100.0/24"
-  availability_zone = "ap-southeast-1a"
-
-  tags = {
-    Name = "hangaramit_public_subnet_1"
-  }
+# 운영환경
+module "prd_custome_vpc" {
+  source = "./custom_vpc"
 }
-
-resource "aws_subnet" "hangaramit_private_subnet_1" {
-  vpc_id            = aws_vpc.default.id
-  cidr_block        = "10.0.101.0/24"
-  availability_zone = "ap-southeast-1a"
-  tags = {
-    Name = "hangaramit_private_subnet_1"
-  }
-}
-
