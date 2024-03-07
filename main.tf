5acabf580df9d08ed92e3c971db91587a038ac06
tingting3355 <skysorahwi@gamil.com>
2024-03-07 11:12:41 +0900

    Create subnet

--- main.tf
+++ main.tf
@@ -15,4 +15,45 @@ provider "aws" {
 # Create a VPC
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main"
+  }
+}
+
+# Create a Public Subnet
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.100.0/24"
+
+  tags = {
+    Name = "hangaramit_public_subnet_1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.101.0/24"
+
+  tags = {
+    Name = "hangaramit_public_subnet_2"
+  }
+}
+
+# Create a Private Subnet
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.110.0/24"
+
+  tags = {
+    Name = "hangaramit_private_subnet_1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.111.0/24"
+
+  tags = {
+    Name = "hangaramit_private_subnet_2"
+  }
 }
